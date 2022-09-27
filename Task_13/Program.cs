217f711fa04a73bb83fd4b6f58c079e5bc9ad973
ChicagoADM <112687685+ChicagoADM@users.noreply.github.com>
2022-09-27 19:32:08 +0300

    Update Program.cs

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -10,3 +10,27 @@ if (str.Length > 2)
 else {
   Console.WriteLine("Третьей цифры нет");
 } 
+
+//___________________________________________________________________________________________
+void Zadacha13()
+{
+    // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+    Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number > 99)
+{
+    while(number > 999)
+    {
+        number /= 10;
+        
+    }
+    Console.WriteLine("третья цифра числа " + number%10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
+}
+Zadacha13();
