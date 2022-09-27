c2563a5f2d98fef5e8ae4ab97fb035888b791fe9
ChicagoADM <112687685+ChicagoADM@users.noreply.github.com>
2022-09-27 18:37:44 +0300

    Update Program.cs

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -4,3 +4,21 @@ Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 string str = number.ToString();
 Console.WriteLine($"Второе число: {str[1]}");
+
+//_________________________________________________________________________
+void Zadacha10()
+{// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine("Вторая цифра числа: " + (number/10%10));
+}
+else
+{
+    Console.WriteLine("Введено не трёхзначное число");
+}
+
+}
+Zadacha10();
